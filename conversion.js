b24a702022858eea8f971fd9033216b3b86271ea
Neamat-bge <neamat.bge@gmail.com.com>
2021-08-01 16:13:03 +0600

    variable type identify

---
+++ conversion.js
@@ -0,0 +1,38 @@
+// var firstName = 'Neamat';
+// var lastName = 'Ullah';
+// var fullName = firstName + ' ' + lastName;
+
+// console.log(fullName)
+
+// var first = 'Ariana';
+// var last = 'Grande';
+// var name = first + ' ' + last;
+
+
+// console.log(name);
+
+// var onionPrice = '100';
+// var eggPrice = '40';
+// var totalPrice = onionPrice + eggPrice;
+// console.log(totalPrice);
+
+// integer 
+
+var sunglass = 2;
+
+console.log(sunglass);
+
+// Float/Vognangso 
+
+var shoeBata = 99.99;
+
+// Interger conversion: string to number 
+
+var onionPrice = '100.42';
+var eggPrice = '40.70';
+var onionPriceNumber = parseInt(onionPrice);
+console.log(onionPriceNumber);
+
+// Float Conversion: String to Float:
+var eggPriceNumber = parseFloat(eggPrice);
+console.log(eggPriceNumber);
---
+++ variable-type.js
@@ -0,0 +1,17 @@
+// Variable types Identification: 
+
+
+var tShirtPrice = 200;
+var brandName = 'Easy';
+var isOriginal = false;
+
+console.log(typeof tShirtPrice);
+console.log(typeof brandName);
+console.log(typeof isOriginal);
+
+var isRomantic
+console.log(isRomantic)
+
+
+var isRomantic = true;
+console.log(isRomantic)
