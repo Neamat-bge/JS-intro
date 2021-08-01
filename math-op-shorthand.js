2a0aff53d87e30665f507c87af2af57404cdf4cc
Neamat-bge <neamat.bge@gmail.com.com>
2021-08-01 15:17:48 +0600

    Math opeation shorthand practice

---
+++ math-op-shorthand.js
@@ -0,0 +1,42 @@
+var price1 = 31;
+var price2 = 20;
+
+var sum = price1 + price2;
+var difference = price1 - price2;
+var multiplication = price1 * price2;
+var division = price1 / price2;
+
+// console.log(sum);
+// console.log(difference);
+// console.log(division);
+// 
+
+
+// short hand 
+
+// var newPrice1 = price1 + 10;
+// var price1 = price2 + 10;
+
+// price1 += 10;
+// console.log(price1);
+
+
+// price2 = price2 - 10;
+// price2 -= 10;
+
+// console.log(price2);
+
+var age = 14;
+// age = age + 1;
+// another way to write 
+// age += 1;
+
+// age++;
+
+var love = 100;
+love = love - 1;
+love -= 1;
+love--;
+
+console.log(love);
+
